f534d6193b759d21d4070a8ebb67bdcc5f89f87a
saurabhsatasia <saurabhsatasia@icloud.com>
2021-01-12 03:41:57 +0100

    created scrapper.py
    all the feature columns extracted successfully

--- book.py
+++ book.py
@@ -1,13 +1,23 @@
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import MinMaxScaler
+from scrapper import book_scrape
 
-
-def read_csv(file_path):
+def scrape(file_path):  # location of books_2000.csv
+    # df = book_scrape(num_books=1200)
+     # ========== OR ============
     df = pd.read_csv(file_path)
     return df
 
+
 def data_clean(df):
+    df.drop("Unnamed: 0", axis=1, inplace=True)
+    df.drop_duplicates(inplace=True)
+    
+    df['num_rating'] = df['num_rating'].astype(str)
+    df['num_rating'] = df['num_rating'].apply(lambda x: x.replace(' ratings', ''))
+    df['num_rating'] = df['num_rating'].astype(object)
+
     ## sparse minirating column to get avg_rating and num_ratings
     df['avg_rating'] = df['minirating'].apply(lambda x: (x.split('— ')[0]))
     df['avg_rating'] = df['avg_rating'].apply(lambda x: x.replace(' avg rating', ''))
@@ -17,6 +27,7 @@ def data_clean(df):
     df['num_ratings'] = df['num_ratings'].apply(lambda x: x.replace(' ratings', ''))
 
     return df
+
 
 def preprocessing(df):
     df.avg_rating = pd.to_numeric(df.avg_rating, errors="coerce")
@@ -34,7 +45,7 @@ def analyse(df):
     pass
 
 def main():
-    df = read_csv('Best_Books_of_the_Decade__2000s__6922_books_.csv')
+    df = scrape('books_2000.csv')
     clean_df = data_clean(df)
     processed_df = preprocessing(clean_df)
     analyse(processed_df)
